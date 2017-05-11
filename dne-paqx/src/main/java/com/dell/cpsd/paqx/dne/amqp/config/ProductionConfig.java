1e04b9df00a234a606043701f79977f4bf607a1b
Catherine Fu <catherine.fu@vce.com>
2017-05-11 16:01:22 -0500

    ESTS-127118:Setup TLS Deployment for RabbitMQ messaging (#6)
    * ESTS-127118:Setup TLS Deployment for RabbitMQ messaging
    
    * ESTS-127118:Setup TLS Deployment for RabbitMQ messaging
    
    * ESTS-127118:Setup TLS Deployment for RabbitMQ messaging

--- dne-paqx/src/main/java/com/dell/cpsd/paqx/dne/amqp/config/ProductionConfig.java
+++ dne-paqx/src/main/java/com/dell/cpsd/paqx/dne/amqp/config/ProductionConfig.java
@@ -5,6 +5,9 @@
 package com.dell.cpsd.paqx.dne.amqp.config;
 
 import com.dell.cpsd.common.rabbitmq.connectors.RabbitMQCachingConnectionFactory;
+import com.dell.cpsd.common.rabbitmq.connectors.TLSConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,20 +16,20 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * Production configuration of AMQP
- * <p>
- * Copyright &copy; 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
- * Dell EMC Confidential/Proprietary Information
- * </p>
  */
 @Configuration
 public class ProductionConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductionConfig.class);
     @Autowired
     private PropertiesConfig propertiesConfig;
 
     @Bean
     @Qualifier("rabbitConnectionFactory")
     public ConnectionFactory productionCachingConnectionFactory() {
-        final com.rabbitmq.client.ConnectionFactory connectionFactory = new com.rabbitmq.client.ConnectionFactory();
+        LOGGER.info("rabbit Connection properties:  sslenabled:{}, host:{}, port:{}, tlsVersion:{}", propertiesConfig.isSslEnabled(),
+                propertiesConfig.keyStorePassPhrase(), propertiesConfig.rabbitHostname(),
+                propertiesConfig.rabbitPort(), propertiesConfig.tlsVersion());
+        final com.rabbitmq.client.ConnectionFactory connectionFactory = new TLSConnectionFactory(propertiesConfig);
         return new RabbitMQCachingConnectionFactory(connectionFactory, propertiesConfig);
     }
 }
