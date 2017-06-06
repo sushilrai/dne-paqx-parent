f3c9d0c484f0cdb718aed3bda6c6a1938b00488d
morea3 <Amit.More@emc.com>
2017-06-06 20:00:40 +0530

    Update test_dne_paqx_docker.py (#47)
    * Update test_dne_paqx_docker.py
    
    updating the command "docker network inspect bridge" to "docker network inspect delldnepaqxweb_default" as per inputs from developers
    
    * Update test_dne_paqx_docker.py
    
    removed extra new line

--- test-packs/test_suites/dne_paqx_parent/test_dne_paqx_docker.py
+++ test-packs/test_suites/dne_paqx_parent/test_dne_paqx_docker.py
@@ -1,7 +1,6 @@
 import af_support_tools
 import os
 import pytest
-
 @pytest.fixture(scope="module", autouse=True)
 def load_test_data():
     # Set config ini file name
@@ -15,7 +14,6 @@ def load_test_data():
     username = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='username')
     global password
     password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='password')
-
 #####################################################################
 # These are the main tests.
 #####################################################################
@@ -24,7 +22,6 @@ def load_test_data():
 @pytest.mark.parametrize("container_name", [
 	("symphony-dne-paqx")
 ])
-
 def test_verify_docker_container(container_name):
     """
     Description        :       This method tests docker service for a container
@@ -34,9 +31,7 @@ def test_verify_docker_container(container_name):
     Parameters  	:       1. container_name       -       Name of the container(STRING)
     Returns             :       None
     """
-    
     assert verify_docker_network(container_name), 'test failed: ' + container_name + ' docker service is not running'
-
 def verify_docker_network(container_name):
     """
     Description 	:       This method will check docker network inspect bridge and host commands to verify if the container is using docker networking service
@@ -44,11 +39,10 @@ def verify_docker_network(container_name):
     Returns             :       0 or 1 (Boolean)
     """
     test_status = "pass"
-    cmd = 'docker network inspect bridge |  grep ' + str(container_name)
+    cmd = 'docker network inspect delldnepaqxweb_default |  grep ' + str(container_name)
     output = af_support_tools.send_ssh_command(host=ipaddress, username=username, password=password, command=cmd, return_output=True).strip()
     if (container_name not in output):
         test_status = "fail"
-
     cmd = 'docker network inspect host |  grep ' + str(container_name)
     output = af_support_tools.send_ssh_command(host=ipaddress, username=username, password=password, command=cmd, return_output=True).strip()
     if (container_name in output):
