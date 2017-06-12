08ea5e2b06b09f7a3995ef2dba9a3148287d1279
David Russell <David.R@Dell.com>
2017-06-12 17:17:18 +0100

    Updating tests (#51)
    * Updating tests
    (1) to take into account new 'docker network' configuration
    (2) to adhere the code to the latest coding-guidelines
    
    * Improving exception handling code by passing exception upto calling test
    
    * Further Improving exception handling code after review
    
    * corrected a number of typos

--- test-packs/test_suites/dne_paqx_parent/test_dne_paqx_TLS.py
+++ test-packs/test_suites/dne_paqx_parent/test_dne_paqx_TLS.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+!/usr/bin/python
 # Copyright Â© 01 April 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
 #
 # Author: russed5
-# Revision: 1.1
+# Revision: 1.2
 # Code Reviewed by:
-# Description:
+# Description: Verify that the DNE-PAQX service communicates over TLS to the Rabbitmq AMQP bus
 
 import pytest
 import requests
@@ -35,56 +35,76 @@ def load_test_data():
      cli_password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS',
                                                               property='password')
 
-     # RMQ Details
-     global rmq_username
-     rmq_username = cpsd.props.rmq_username
-     global rmq_password
-     rmq_password = cpsd.props.rmq_password
-     global port
-     port = cpsd.props.rmq_port
 
 ##############################################################################################
-
 
 @pytest.mark.dne_paqx_parent_mvp
 @pytest.mark.dne_paqx_parent_mvp_extended
 def test_dnePAQX_container_using_AMQP_over_TLS():
-    # this test uses the  processId for the dne paqx container
-    # to verify that the network connection to rabbitmq for that container
-    # is using the rabbitmq ssl port
+    """ Verify DNE-PAQX service communicates with rabbitmq over TLS
 
-    ssl_listen_port = getRabbitmqSSLport(ipaddress)
+    this test uses the  processId of the dne paqx container
+    to verify that the network connection to rabbitmq for that container
+    is using the rabbitmq tls port"""
 
-    connectionPorts = getContainerRabbitPorts('cpsd-dne-paqx')
+    try:
 
-    connectionsText = connectionPorts.splitlines()     # shouldn't be more then 1 line returned, but just in case
-    assert connectionsText, 'no rabbitmq connection detected for DNE PAQX'
-    for line in connectionsText:
-        assert ssl_listen_port in line, "The DNE PAQX is not using the SSL Port"
+        ssl_listen_port = getRabbitmqSSLport(ipaddress)
+        assert ssl_listen_port, "Error : It was not possible to retrieve the rabbitmq SSL port "
 
+        # For the dne paqx, use 'docker network inspect' to ascertain its docker network IP address
+        # and use this ip address with the 'netstat' command to find the dne-rabbitmq port-pairing.
+        # example return text would be :
+        # tcp6       0      0 172.17.0.1:5671         172.21.0.2:47338        ESTABLISHED 987/beam.smp
+        connectionPorts = getDockerNetworkConnectionPorts('dnepaqxweb')
+
+        # shouldn't be more then 1 line returned, but just in case we split them
+        connectionsText = connectionPorts.splitlines()
+
+        # if no lines were returned, then there is an error
+        assert connectionsText, 'no rabbitmq connection detected for DNE PAQX'
+
+        for line in connectionsText:
+            # check that it is the correct TLS port that is listed for the rabbitmq-dne connection
+            assert ssl_listen_port in line, "The DNE PAQX is not using the SSL Port"
+
+
+    except Exception as err:
+
+        # Return code error (e.g. 404, 501, ...)
+        print(err)
+        print('\n')
+        raise Exception(err)
 
 #####################################################################################################
 
 @pytest.mark.dne_paqx_parent_mvp
 @pytest.mark.dne_paqx_parent_mvp_extended
 def test_DNEpaqx_AMQP_data_is_encrypted():
-    #
-    # 1. get the processId associated with the DNE paqx
-    # 2. use the returned processId to filter the output of the 'lsof' utility
-    # 3. check that the listed AMQP connection is secure by checking for the phrase 'amqps' in the listing
-    #
-    commandGetContainerProcessID = "ps -ef | grep [j]ava | grep dne-paqx | awk '{print $2}' "
+    """ Verify DNE-PAQX network status reads amqps.
 
-    processId = af_support_tools.send_ssh_command(
-        host=ipaddress,
-        username=cli_username,
-        password=cli_password,
-        command=commandGetContainerProcessID,
-        return_output=True)
+    1. Get the processId associated with the DNE paqx
+    2. Use the returned processId to filter the output of the 'lsof' utility
+    3. Check that the listed AMQP connection is secure by checking for the phrase 'amqps' in the listing"""
+    #
+
+    # use the getDockerNetworkConnectionPorts fucntion , with the 'processId' flag, to indicate the
+    # processID should be returned.
+    processId = getDockerNetworkConnectionPorts('dnepaqxweb', "processId")
+    assert processId, "There was no PID found for the DNE PAQX"
+    # use the getDockerNetworkConnectionPorts function , without a specific flag, to indicate the
+    # connection ports should be returned. This is returned in the form :
+    # tcp6       0      0 172.17.0.1:5671         172.21.0.2:47338        ESTABLISHED 987/beam.smp
+    connectionPortText = getDockerNetworkConnectionPorts('dnepaqxweb')
+    assert connectionPortText, "There was no rabbitmq connection found for the DNE PAQX"
+
+    # split the dne-port out of the returned text, to be used in the follwoing 'lsof' command
+    peerPort = getPeerPortFromNetstatOutput(connectionPortText)
+    assert peerPort, "The peer port for the rabbitmq connection could not be obtained"
 
     # using lsof, we expect to see output similar to below and are particularly interested in the 'amqps' text
     #    java    4969 root   21u  IPv6    57839    0t0    TCP localhost:48786->localhost:amqps (ESTABLISHED)
-    commandCheckEncryption = "lsof -p " + processId.rstrip() + " | grep [E]STABLISHED | grep amqps"
+    commandCheckEncryption = "lsof -p " + processId.rstrip() + " | grep [E]STABLISHED | grep amqps" + " | grep " + peerPort.rstrip()
 
     return_text = af_support_tools.send_ssh_command(
         host=ipaddress,
@@ -97,118 +117,81 @@ def test_DNEpaqx_AMQP_data_is_encrypted():
 
 #####################################################################################################
 
-def getQueueDetails(queue):
-#
-# query specific rabbitmq queue to return the connection name
-# this name is of the form 'host:port -> host:port'
-#
-    # query rabbitmq for details of 'queue'
-    apipath = "/api/queues/%2f/" + queue
-    my_url = 'http://' + ipaddress + ':15672' + apipath
-
-    try:
-        url_response = requests.get(my_url, auth=(rmq_username,rmq_password))
-        url_response.raise_for_status()
-
-    except requests.exceptions.HTTPError as err:
-        # Return code error (e.g. 404, 501, ...)
-        print(err)
-        print('\n')
-        assert False
-
-    except requests.exceptions.Timeout as err:
-        # Not an HTTP-specific error (e.g. connection refused)
-        print(err)
-        print('\n')
-        assert False
-
-    else:
-        # 200
-        data = json.loads(url_response.text)
-
-    return data
-
-
-#####################################################################################################
-
 def getRabbitmqSSLport(ip):
-#
-# retrieve the rabbitmq ssl port via the rabbitmq http api
-#
+    """ Retrieve the rabbitmq ssl port via the rabbitmq http api."""
 
     apipath = "/api/overview"
     my_url = 'http://' + ipaddress + ':15672' + apipath
 
     try:
-        url_response = requests.get(my_url, auth=(rmq_username,rmq_password))
+        url_response = requests.get(my_url, auth=(cpsd.props.rmq_username,cpsd.props.rmq_password))
         url_response.raise_for_status()
 
-    except requests.exceptions.HTTPError as err:
+        # 200
+        data = json.loads(url_response.text)
+        for i in data['listeners']:
+            if i['protocol'] == 'amqp/ssl':
+                ssl_port = i['port']
+
+        if ssl_port:
+            portToReturn = str(ssl_port)
+        else:
+            portToReturn = None
+
+        return portToReturn
+
+    except Exception as err:
         # Return code error (e.g. 404, 501, ...)
         print(err)
         print('\n')
-        assert False
+        raise Exception(err)
 
-    except requests.exceptions.Timeout as err:
-        # Not an HTTP-specific error (e.g. connection refused)
-        print(err)
-        print('\n')
-        assert False
-
-    else:
-        # 200
-        data = json.loads(url_response.text)
-        ssl_port = '9999'       #non-valid number in case no ssl port found
-
-        for i in data['listeners']:
-            if i['protocol'] == 'amqp/ssl' :
-                ssl_port = i['port']
-
-        portToReturn = str(ssl_port)
-        assert ssl_port != '9999', "Error : Rabbitmq is not configured for TSL "
-
-    return portToReturn
 
 #########################################################################################################
 
 def getPeerPortFromNetstatOutput(netstatText):
-    #
-    # expected format of text is :
-    #    ::1:56814 ::1:5671
+    """ A function to extract and return the non-rabbitmq peer port from specific netstat output text.
 
-    portstext = netstatText.split(' ')
-    peer_port_raw = portstext[0].split(':')
-    peer_port = peer_port_raw[-1]
+    the expected format of text is :
+    tcp6       0      0 172.17.0.1:5671         172.21.0.2:47338        ESTABLISHED 987/beam.smp
+    In this sample case, '47388' would be returned """
+
+    portstext = netstatText.split(':')
+    peer_port_raw = portstext[2].split(' ')
+    peer_port = peer_port_raw[0]
 
     return peer_port
 
 ######################################################################################################
 
-def getContainerRabbitPorts(containerName):
+def getDockerNetworkConnectionPorts(containerName, label="ports"):
+    """ A function to determine the docker network ipaddress of a PAQX container.
 
-    #ssl_listen_port = getRabbitmqSSLport(ipaddress)
+    The docker network commands are used to determine the IP Address of the PAQX container.
+    'netstat' is then used to return the connection details for that IP Address.
+    If a second parameter is  passed to this function, and is equal to 'processId', then the connection details
+    are further broken down to return just the processID.
+    Eg. from the sample data below, the process ID = 987 would be returned
+    tcp6       0      0 172.17.0.1:5671         172.21.0.2:47338        ESTABLISHED 987/beam.smp"""
 
-    commandGetContainerId = "docker ps | grep " + containerName + " |cut -f1 -d ' '"
-    containerId = af_support_tools.send_ssh_command(
+    commandGetNetworkId = "docker network list | grep " + containerName + " |cut -f1 -d ' '"
+    networkId = af_support_tools.send_ssh_command(
                 host=ipaddress,
                 username=cli_username,
                 password=cli_password,
-                command=commandGetContainerId,
+                command=commandGetNetworkId,
                 return_output=True)
-    print(containerId)
 
-    commandGetContainerProcessID = "ps -ef | grep '[c]ontainer.id=" + containerId.rstrip() + "' | grep java | awk '{print $2}' "
-    print(commandGetContainerProcessID)
-    processId = af_support_tools.send_ssh_command(
+    commandGetContainerIPAddress = "docker network inspect " + networkId.rstrip() +\
+                                   " | grep IPv4 | awk '{print $2}'  | cut -f2 -d '\"' | cut -f1 -d '/' "
+    ipAddressText = af_support_tools.send_ssh_command(
                 host=ipaddress,
                 username=cli_username,
                 password=cli_password,
-                command=commandGetContainerProcessID,
+                command=commandGetContainerIPAddress,
                 return_output=True)
-    print(processId)
 
-    commandGetContainerConnectionPort = "netstat -tupn | grep " + processId.rstrip() + " | grep -v 8500"
-    print(commandGetContainerConnectionPort)
+    commandGetContainerConnectionPort = "netstat -tupn | grep " + ipAddressText.rstrip() + " | grep -v 8500"
     connectionPorts = af_support_tools.send_ssh_command(
                 host=ipaddress,
                 username=cli_username,
@@ -216,6 +199,12 @@ def getContainerRabbitPorts(containerName):
                 command=commandGetContainerConnectionPort,
                 return_output=True)
 
-    print(connectionPorts)
+    if label == "processId" :
+        processtext = connectionPorts.split("ESTABLISHED")
+        process_raw = processtext[1].split('/')
+        processId = process_raw[0]
+        return processId
+    else:
+        return connectionPorts
 
-    return connectionPorts
+
