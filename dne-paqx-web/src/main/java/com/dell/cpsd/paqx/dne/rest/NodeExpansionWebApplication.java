1b50f61c8f598257f2e5378764ae3e1c06562636
Kushagra Mahajan <kushagra.mahajan@dell.com>
2017-09-29 07:15:47 +0100

    Added H2 Web Server for testing (#239)

--- dne-paqx-web/src/main/java/com/dell/cpsd/paqx/dne/rest/NodeExpansionWebApplication.java
+++ dne-paqx-web/src/main/java/com/dell/cpsd/paqx/dne/rest/NodeExpansionWebApplication.java
@@ -7,11 +7,14 @@ package com.dell.cpsd.paqx.dne.rest;
 
 import com.dell.cpsd.hdp.capability.registry.client.lookup.config.CapabilityRegistryLookupManagerConfig;
 import com.dell.cpsd.paqx.dne.amqp.config.ServiceConfig;
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -21,5 +24,17 @@ public class NodeExpansionWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NodeExpansionWebApplication.class, args);
+
+        //TODO: Remove this its only for testing
+        try
+        {
+            Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8555").start();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+
     }
 }
