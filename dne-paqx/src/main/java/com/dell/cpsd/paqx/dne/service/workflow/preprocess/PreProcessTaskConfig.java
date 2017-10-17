bf8372750f0b3d8d616dc8e42fdd2344e7ec911a
Jack Kenefick <jkenefick@eircom.net>
2017-10-17 16:00:41 +0100

    Re-enabling the findScaleIO workflow (#307)

--- dne-paqx/src/main/java/com/dell/cpsd/paqx/dne/service/workflow/preprocess/PreProcessTaskConfig.java
+++ dne-paqx/src/main/java/com/dell/cpsd/paqx/dne/service/workflow/preprocess/PreProcessTaskConfig.java
@@ -39,7 +39,7 @@ public class PreProcessTaskConfig {
                 "pingIdrac",
                 "configureObmSettings",
                 "configureBootDeviceIdrac",
-                //"findScaleIO",
+                "findScaleIO",
                 "findVCluster",
                 "findProtectionDomain");
 
